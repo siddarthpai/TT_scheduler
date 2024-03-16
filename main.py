3c534cd688c749c3cd0441a134d189f87e8dc01e
Siddarth Pai <siddarthdpai@gmail.com>
2024-03-16 10:16:02 +0530

    first commit

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import streamlit as st
+import json
 import pandas as pd
+
+def load_subjects():
+    with open("subjects.json", "r") as file:
+        return json.load(file)
 
 def main():
     st.title("Time Table Scheduler")
     
     semesters = ['Sem I', 'Sem II', 'Sem III', 'Sem IV', 'Sem V', 'Sem VI', 'Sem VII', 'Sem VIII']
     selected_semester = st.selectbox("Select a semester:", semesters)
+
+    subjects_data = load_subjects()
+    
+
+    subjects = subjects_data.get(selected_semester, [])
+
     num_teachers = {}
-    if selected_semester == 'Sem VI':
+    if subjects: 
         with st.expander("Enter number of teachers for each subject"):
             num_teachers = {}
-            for subject in ['UE21CS341B Compiler Design', 'UE21CS351B Cloud Computing',
-                            'UE21CS352B Object Oriented Analysis & Design using Java',
-                            'Elective A', 'Elective B']:
+            for subject in subjects:
                 num_teachers[subject] = st.number_input(f"Enter the number of teachers for {subject}:",
                                                         min_value=0, value=1)
             st.write("Number of teachers for each subject:")
             st.write(num_teachers)
+    
     num_sections = st.number_input("Enter the number of sections:", min_value=1, value=3)
     days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     classes = ['Period 1', 'Period 2', 'Period 3', 'Period 4', 'Period 5', 'Period 6', 'Period 7', 'Period 8', 'Period 9']
